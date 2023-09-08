03834447ac79518b240855241466704757ead8af
rlp2mz <Paulina.Szymanska@aptiv.com>
2023-09-08 12:11:30 +0200

    Added Palindrome exercises

---
+++ src/shortTasks/Palindrome.java
@@ -0,0 +1,25 @@
+package src.shortTasks;
+
+import java.util.ArrayList;
+
+//A palindrome is a word which reads the same backward or forward. 'abcba' is a palindrome.
+//        Write a method that detects if a string is a palindrome.
+//        Tip: Use word.charAt(i) to get the character at position i.
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("eyte"));
+        System.out.println(isPalindrome("abd"));
+        System.out.println(isPalindrome("eye"));
+        System.out.println(isPalindrome("eyye"));
+    }
+
+    public static boolean isPalindrome(String word) {
+        int length = word.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (word.charAt(i) != word.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
